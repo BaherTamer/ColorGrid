d36265ac6b0f8199dde43bdcc3bd5d9672d91b27
Baher Tamer <baher.appledev@gmail.com>
2023-09-25 15:45:11 +0300

    Create CGPicker.swift

---
+++ Sources/ColorGrid/Views/CGPicker.swift
@@ -0,0 +1,89 @@
+//
+//  CGPicker.swift
+//
+//
+//  Created by Baher Tamer on 25/09/2023.
+//
+
+import SwiftUI
+
+/** A control used to select a color from the provided array of colors.
+ 
+ You create a picker by providing an array of colors, a selection binding to a color, a number of columns of the colors grid, and a picker symbol.
+ 
+ ``` swift
+ struct ContentView: View {
+    @State private var selectedColor: Color = .red
+    private let colors: [Color] = [.red, .orange, .yellow, .green, .blue, .indigo, .purple, .pink, .brown]
+ 
+    var body: some View {
+        CGPicker(colors: colors, selection: $selectedColor, columns: 6, icon: .circle)
+    }
+ }
+ ```
+ 
+ By default the picker initialize `columns` with `6` and `icon` with `circle`.
+ 
+ ``` swift
+ CGPicker(colors: colors, selection: $selectedColor)
+ ```
+ 
+ */
+public struct CGPicker: View {
+    
+    @Binding public var selection: Color
+    
+    private let colors: [Color]
+    private let columns: [GridItem]
+    private let icon: CGIcon
+    
+    /// Creates an instance that selects a color.
+    ///
+    /// - Parameters:
+    ///   - colors: A collection of colors used as the source for the picker
+    ///   - selection: A `Binding` to a variable that determines the currently-selected color
+    ///   - columns: A number of columns for the picker grid view.
+    ///   - icon: A symbol icon for each color shape in the picker.
+    public init(colors: [Color], selection: Binding<Color>, columns: Int = 6, icon: CGIcon = .circle) {
+        self._selection = selection
+        
+        self.colors = colors
+        self.columns = Array(repeating: GridItem(.flexible()), count: columns)
+        self.icon = icon
+    }
+    
+}
+
+// MARK: - View Components
+extension CGPicker {
+    
+    public var body: some View {
+        LazyVGrid(columns: self.columns, spacing: 16) {
+            ForEach(self.colors, id: \.self) { color in
+                ZStack {
+                    if color == self.selection {
+                        highlightImage
+                    }
+                    
+                    Image(systemName: self.icon.fill)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32)
+                        .foregroundColor(color)
+                }
+                .frame(width: 48, height: 48)
+                .onTapGesture {
+                    self.selection = color
+                }
+            }
+        }
+    }
+    
+    private var highlightImage: some View {
+        Image(systemName: self.icon.stroke)
+            .resizable()
+            .scaledToFit()
+            .foregroundColor(Color(UIColor.systemGray3))
+    }
+    
+}
